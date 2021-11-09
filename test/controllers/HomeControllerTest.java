d66b6111173e9288d80c5732357c0f713c9e1c02
bkrupali <krupalibhatt1711@gmail.com>
2021-11-08 22:55:31 -0500

    Made changes to testIndex().

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,19 +1,35 @@
 package controllers;
 
+import static org.junit.Assert.assertEquals;
+
+
+
+
+
+import static play.mvc.Http.Status.OK;
+
+
+
+
+import static play.test.Helpers.GET;
+import static play.test.Helpers.route;
+
+
 import org.junit.Test;
+
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 
-import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
+
+
 
 public class HomeControllerTest extends WithApplication {
 
+
+	
     @Override
     protected Application provideApplication() {
         return new GuiceApplicationBuilder().build();
@@ -28,5 +44,6 @@ public class HomeControllerTest extends WithApplication {
         Result result = route(app, request);
         assertEquals(OK, result.status());
     }
+	
 
 }
