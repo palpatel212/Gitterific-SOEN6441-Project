c6b351c8c98bcc256ce566f7f4c5486a174fbf07
Pal Patel <58902379+palpatel212@users.noreply.github.com>
2021-11-12 02:20:49 -0500

    Update HomeControllerTest.java
    Unit Testing

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,45 +1,43 @@
 package controllers;
 
 import static org.junit.Assert.assertEquals;
-
-
-
-
-
-import static play.mvc.Http.Status.OK;
-
-
-
-
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
-
-
 import org.junit.Test;
-
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.route;
+
 
 
 
 public class HomeControllerTest extends WithApplication {
 
-
-	
     @Override
     protected Application provideApplication() {
         return new GuiceApplicationBuilder().build();
     }
 
     @Test
-    public void testIndex() {
+    public static void testdisplayrepo() {
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(GET)
-                .uri("/");
+                .uri("/repo");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+    }
+    
+    @Test
+    public void testcollaborators() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/user/:id ");
 
         Result result = route(app, request);
         assertEquals(OK, result.status());
