541cfeaf260d741bdec9e27e83a8a45569b0f88a
juhipatel <juhibirjupatel@gmail.com>
2021-11-20 12:57:25 -0500

    Modified commit functions

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -45,34 +45,19 @@ import play.mvc.Http;
 import play.mvc.Result;
 
 /**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
+ * Defines methods that renders different views
  */
- 
 public class HomeController extends Controller {
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    
 	
-	public List<Commits> com = new ArrayList<Commits>();
-	public List<Committer> committers = new ArrayList<Committer>();
 	List<Issues> issueList = new ArrayList<Issues>();
 	List<Issues> top20issueList = new ArrayList<Issues>();
 	public ArrayList<String> RepoCollabs;
-	public HashMap<String, Integer> sorted;
-	public List<Integer> additionResult = new ArrayList<Integer>();
-	public List<Integer> deletionResult = new ArrayList<Integer>();
-	public Integer maxAdd;
-	public Integer maxDel;
-	public Integer minAdd;
-	public Integer minDel;
-	public OptionalDouble avgAdd;
-	public OptionalDouble avgDel;
+	
+	
+
 	static RepoData repos;
-	Repository r= new Repository();
+	public Repository r= new Repository();
 	FormFactory formFactory;
 	MessagesApi messagesApi;
 	Form<RepoData> repoForm;
@@ -83,16 +68,30 @@ public class HomeController extends Controller {
 	}
 	
 	public List<String> keywords;
+	
+	 /**
+	   * This method renders index view
+	   * @return Result
+	   */
 	    public Result index() {
         return ok("Hello world");
     }
     
+	    /**
+		   * This method renders search Repo view
+		   * @param request http-Request
+		   * @return Result
+		   */    
     public Result create(Http.Request request) {
     	repoForm = formFactory.form(RepoData.class);
     	System.out.println("In create");
     	return ok(views.html.create.render(repoForm,request,messagesApi.preferred(request)));
     }
-    
+    /**
+	   * This method lists repositories  
+	   * @param request http-Request
+	   * @return Result
+	   */
     public CompletionStage<Result> save(Http.Request request) {
     	
     	Form<RepoData> repoForm = formFactory.form(RepoData.class);
@@ -105,6 +104,11 @@ public class HomeController extends Controller {
     	}).thenApply(repo -> ok(views.html.index.render(repo)));
     }
     
+    /**
+	   * This method lists issues
+	   * @param id Repository Id
+	   * @return Result
+	   */
     public CompletionStage<Result> issues(String id) {
     	
     	for(Repository rd : RepoDetails.repos) {
@@ -119,12 +123,23 @@ public class HomeController extends Controller {
     	
     }
     
+    /**
+	   * This method displays userInfo
+	   * @param login Userlogin
+	   * @return Result
+	   */
     public Result userinfo(String login)
     {
     	User userDetail=UserController.storeUserInfo(UserController.UserApiCall(login));
     	return ok(views.html.user.render(userDetail));
     }
     
+    
+    /**
+	   * This method renders commits view
+	   * @param id RepositoryId
+	   * @return Result
+	   */
     public Result commits(String id) {
     	for(Repository rd : RepoDetails.repos) {
     		if(id.equals(rd.id)) {
@@ -133,10 +148,16 @@ public class HomeController extends Controller {
     		}
     	}
     	
-    	findcommit();
+    	CommitDetails.findcommit(r);
     	
-    	return ok(views.html.commits.render(com));
+    	return ok(views.html.commits.render(CommitDetails.com));
     }
+    
+    /**
+	   * This method renders repo view
+	   * @param id RepoID
+	   * @return Result
+	   */
     
     public Result repo(String id)
     {
@@ -169,60 +190,21 @@ public class HomeController extends Controller {
     	return ok(views.html.RepoView.render(r, top20issueList, RepoCollabs));
     }
     
+    /**
+	   * This method renders commitStats view
+	   * @return Result
+	   */
     public Result commitStats() {
     	
-        
-    	additionResult.clear();
-    	deletionResult.clear();
+    	CommitDetails.commitStatistics();
     	
-    	int count =0;
-    	committers.clear();
-    	Iterator hmIterator = sorted.entrySet().iterator();
-    	while (hmIterator.hasNext()&& count<10) {
-            Map.Entry mapElement = (Map.Entry)hmIterator.next();
-            Committer c = new Committer();
-            String name = (String) mapElement.getKey(); 
-            c.setName(name);
-            Integer commitNum = (Integer) mapElement.getValue();
-            c.setCommitNum(commitNum);
-            committers.add(c);
-            count++;
-        }
-    	
-    	for(Commits c:com) {
-    			JSONObject results = addDelStats(c.commitUrl);
-	    		System.out.println("Result object created.");
-				JSONObject temptStats = (JSONObject)results.get("stats");
-				if(!JSONObject.NULL.equals(temptStats)) {
-					System.out.println("In stats object");
-					Integer addition = (Integer)temptStats.getInt("additions");
-					additionResult.add(addition);
-					Integer deletion = (Integer)temptStats.getInt("deletions");
-					deletionResult.add(deletion);
-				}
-				avgAdd = additionResult
-			            .stream()
-			            .mapToDouble(a -> a)
-			            .average();
-				
-				
-				avgDel = deletionResult
-			            .stream()
-			            .mapToDouble(a -> a)
-			            .average();
-				
-				maxAdd = additionResult.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMax();
-				maxDel = deletionResult.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMax();
-				
-				minAdd = additionResult.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMin();
-				minDel = deletionResult.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMin();	
-				
-				}
-        
-    	
-    	return ok(views.html.commitSats.render(committers,avgAdd,avgDel,maxAdd,maxDel,minAdd,minDel));
+    	return ok(views.html.commitSats.render(CommitDetails.committers,CommitDetails.avgAdd,CommitDetails.avgDel,CommitDetails.maxAdd,CommitDetails.maxDel,CommitDetails.minAdd,CommitDetails.minDel));
     }
     
+    /**
+	   * This method renders issueStatistics view
+	   * @return Result
+	   */
     public CompletionStage<Result> issueStats() {
     	return CompletableFuture.supplyAsync(() -> {
     		List<String> issueTitles = this.issueList.stream().map(i -> i.getTitle()).collect(Collectors.toList());
@@ -238,117 +220,7 @@ public class HomeController extends Controller {
     	}).thenApply(finalMapDescendingOrder -> ok(views.html.issueStats.render(finalMapDescendingOrder)));
     }
     
-public void findcommit() {
-	  	
-    	System.out.println("In find commit");
-		
-  		JSONArray jsonObject = null;
-  		
-  		try {
-  			System.out.println("In URL Builder");
-  			URIBuilder builder = new URIBuilder("https://api.github.com/repos/"+r.login+"/"+r.repoName+"/commits");
-  			builder.addParameter("accept", "application/vnd.github.v3+json");
-  			builder.addParameter("X-RateLimit-Reset", "1350085394");
-  			builder.addParameter("per_page", "10");
-  			
-  			CloseableHttpClient httpclient = HttpClients.createDefault();
+} 
 
-  			HttpResponse resp = null;
-  			
-  			
-  			
-  			HttpGet getAPI = new HttpGet(builder.build());
-  			resp = httpclient.execute(getAPI);
-  			
-  			StatusLine statusLine = resp.getStatusLine();
-  	        System.out.println(statusLine.getStatusCode() + " " + statusLine.getReasonPhrase());
-  	        String responseBody = EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8);
-  	        System.out.println(responseBody.length());
-  	        
-  			try {
-  			     jsonObject = new JSONArray(responseBody);
-  			}catch (JSONException err){
-  			     err.printStackTrace();
-  			}
-  			
-  		} catch (URISyntaxException | IOException | RuntimeException e) {
-  			// TODO Auto-generated catch block
-  			e.printStackTrace();
-  		}
-  		
-  		
-      	
-      	com.clear();
-      	committers.clear();
-     	System.out.println("JSON value"+jsonObject);
-      	System.out.println("In find commit function");
-      	for(int i=0; i<jsonObject.length(); i++) {
-      		Commits obj = new Commits();
-      		System.out.println("Commit object created");
-      		if(!JSONObject.NULL.equals(jsonObject.getJSONObject(i))) {
-  	    		String commitUrl= jsonObject.getJSONObject(i).getString("url");
-  	    		obj.setCommitUrl(commitUrl);
-  	    		System.out.println("url object created");
-  	    		JSONObject temptCommitter = (JSONObject)jsonObject.getJSONObject(i).get("author");
-  	    		String committerName= (String)temptCommitter.getString("login");
-  	    		obj.setCommitterName(committerName);
-  	    		Integer committerId= (Integer)temptCommitter.getNumber("id");
-  	    		obj.setCommitterId(committerId);
-  	    		JSONObject tempt = (JSONObject)jsonObject.getJSONObject(i).get("commit");
-  	    		String commitName= (String)tempt.getString("message");
-  	    		obj.setCommitName(commitName);	
-  	    		obj.updateMap();
-  	    		com.add(obj);
-  	    		
-  	    		
-      		}
-      	}
-      	
-      	HashMap<String, Integer> temp =Commits.countMap.entrySet()
-                .stream()
-                .sorted((i1, i2)
-                            -> i1.getValue().compareTo(
-                                i2.getValue()))
-                .collect(Collectors.toMap(
-                    Map.Entry::getKey,
-                    Map.Entry::getValue,
-                    (e1, e2) -> e1, LinkedHashMap::new));
-      	
 
-      	sorted = temp.entrySet() .stream() .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())) .collect( Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-
-      }
-    
-public static JSONObject addDelStats(String url) {
-		
-		JSONObject jsonObject = null;
-		try {
-			URIBuilder builder = new URIBuilder(url);
-			builder.addParameter("accept", "application/vnd.github.v3+json");
-			CloseableHttpClient httpclient = HttpClients.createDefault();
-
-			HttpResponse resp = null;
-			
-			
-			HttpGet getAPI = new HttpGet(builder.build());
-			resp = httpclient.execute(getAPI);
-			
-			StatusLine statusLine = resp.getStatusLine();
-	        System.out.println(statusLine.getStatusCode() + " " + statusLine.getReasonPhrase());
-	        String responseBody = EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8);
-	        System.out.println(responseBody.length());
-	        
-			try {
-			     jsonObject = new JSONObject(responseBody);
-			}catch (JSONException err){
-			     err.printStackTrace();
-			}
-			
-		} catch (URISyntaxException | IOException | RuntimeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return jsonObject;
-	}
-  }
 
