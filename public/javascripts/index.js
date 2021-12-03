17d269719201aa4c243b7309ad67f4668f73eeaa
jayp-tech <jaybpatel140399@gmail.com>
2021-12-03 18:24:27 -0500

    Appended multiple search results in search page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,23 @@
-
 function search() {
 	let searchSocket = new WebSocket("ws://localhost:9000/socket");
 	var searchKey = document.getElementById("searchTerm").value;
-	
 	let message = {
 		"keyword": searchKey
 	};
-	let msg = JSON.stringify(message);
-	
+	let msg = JSON.stringify(message);	
 	searchSocket.onopen = function(){
 		alert("started");
 		searchSocket.send(msg);
 	}
 	searchSocket.onmessage = function(event) {
 		var response = event.data;
+		var div = document.getElementById('result_contents');
+		
+		
+		var child_div = document.createElement('div');
+		child_div.innerHTML += response;
+		div.prepend(child_div)
+		
 		alert(response);
 	}
 }
