70528f8294aea1d9f8d1d11ed4e2250894371b4e
jayp-tech <jaybpatel140399@gmail.com>
2021-12-04 03:09:59 -0500

    Fixed link issues in the search page and organized search results page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,13 +11,33 @@ function search() {
 	}
 	searchSocket.onmessage = function(event) {
 		var response = event.data;
+		const Res = JSON.parse(response);
+		
 		var div = document.getElementById('result_contents');
-		
-		
+
 		var child_div = document.createElement('div');
-		child_div.innerHTML += response;
-		div.prepend(child_div)
+		console.log(Res.data.length);
 		
+	
+		
+		for(let i=0; i<Res.data.length; i++)
+		{
+			var inner = "<div>"
+			inner += "<div>"
+			const ResObj = Res.data[i];
+			inner += '<p> User: ' + '<a href="http://localhost:9000/user/' + ResObj.login + '">' + ResObj.login+ '</a> </p>';
+			inner += '<p> Repository: ' + '<a href="http://localhost:9000/check/' + ResObj.id + '">' + ResObj.repoName+ '</a> </p>';
+			inner += "<p> Topics: </p>";
+			const ResTopics = Res.data[i].topics;
+			for( let j = 0; j< ResTopics.length; j++)
+			{
+			 inner += '<li>' + '<a href="http://localhost:9000/topicsearch/' + ResTopics[j] + '">' + ResTopics[j] + '</a> </li>';
+			}			
+			inner += " </div> </div>"
+			child_div.innerHTML += inner;
+		}
+		
+		div.prepend(child_div)
 		alert(response);
 	}
 }
